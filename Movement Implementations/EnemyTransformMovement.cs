e2f53b6ab766073eea78fe372d7a8b728c1be34c
Paku <64353979+PakuBK@users.noreply.github.com>
2022-07-31 13:41:26 +0200

    fix velocity issues with transform movement

--- Movement Implementations/EnemyTransformMovement.cs
+++ Movement Implementations/EnemyTransformMovement.cs
@@ -2,7 +2,27 @@ using UnityEngine;
 
 public class EnemyTransformMovement : MonoBehaviour, IEnemyMovement
 {
+    private Rigidbody2D rb;
+    private Collider2D cld;
+    [SerializeField]
+    private LayerMask ground;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        cld = GetComponent<Collider2D>();
+    }
+
     public void Move(int direction, float movementSpeed) {
         transform.Translate((Vector3.right * direction) * movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (cld.IsTouchingLayers(ground))
+        {
+            if (!rb.isKinematic)
+            {
+                rb.isKinematic = true;
+            }
+        }
     }
 }
