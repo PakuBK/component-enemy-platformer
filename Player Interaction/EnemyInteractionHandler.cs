ed88242d72f98e550744807f781ce95dd6b0c98c
Paku <64353979+PakuBK@users.noreply.github.com>
2022-07-31 13:43:23 +0200

    add classic player interaction

---
+++ Player Interaction/EnemyInteractionHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInteractionHandler : MonoBehaviour
+{
+    // Detect if Enemy touches Player
+    // Detect if Player jumped on Enemy
+
+    private Transform playerRef;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.collider.CompareTag("Player")) {
+            HandlePlayerCollision(other.transform);
+        }
+    }
+
+    private void HandlePlayerCollision(Transform player) {
+        playerRef = player;
+        // Check if Player is on top of Enemy
+        if (player.position.y > transform.position.y){
+            PlayerOnTop();
+        }
+        else {
+            PlayerTouchesEnemy();
+        }
+    }
+
+    private void PlayerOnTop() {
+        Debug.Log("Player on Top of Enemy");
+        gameObject.SetActive(false);
+    }
+
+    private void PlayerTouchesEnemy() {
+        Debug.Log("Player touches Enemy");
+        playerRef.gameObject.SetActive(false);
+    }
+}
